827cb2c6000de2d9e6e2b4d6f9db1999ff3ab033
HaykoAnderson <88984099+HaykoAnderson@users.noreply.github.com>
2026-07-03 19:23:35 +0200

    Add debug output in WrapObject function
    Added debug print statement to WrapObject function.

--- src/ESP.lua
+++ src/ESP.lua
@@ -1471,6 +1471,7 @@ local UtilityFunctions = {
 		assert(self, "EXUNYS_ESP > UtilityFunctions.WrapObject - Internal error, unassigned parameter \"self\".")
 
 		if pcall(gethiddenproperty, Object, "PrimaryPart") then
+			print(" --DEBUG : "..Object.Name.." WHICH IS A "..typeof(Object).." IN /"..Object.Parent.Parent.Parent.Name.."/"..Object.Parent.Parent.Name.."/"..Object.Parent.Name.."/"
 			Object = Object.PrimaryPart
 		end
 
